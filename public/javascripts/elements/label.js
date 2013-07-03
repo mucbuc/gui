1d346229c31282f3f5c1d642bd72c4d11783f340
markymark <smallitle@hotmail.com>
2013-07-03 03:36:03 -0700

    autosize label to text width

--- public/javascripts/elements/label.js
+++ public/javascripts/elements/label.js
@@ -9,25 +9,31 @@ objective:
   function Label( controller ) {
     
     var instance = this
-      , model = controller.model;
+      , model
+      , width = 0;
     
     Element.call( this, controller );
+
+    // set model 
+    update();
     
+    // listeners
     controller.once( 'unload', function() { 
       controller.removeListener( 'render', render );
       controller.removeListener( 'update', update );
     } );
-    
     controller.on( 'render', render );
     controller.on( 'update', update );
     
+    // callbacks
     function update() {
       model = controller.model;
+      SetDrawTextContext( instance.color, instance.fontSize, false );
+      width = getTextWidth( model, controller.context );
     } 
-    
     function render() {
       var bounds = instance.bounds;
-      DrawText( bounds.left, bounds.bottom, instance.color, model, instance.fontSize, false);
+      DrawText( bounds.left, bounds.bottom, instance.color, model, instance.fontSize, false, width );
     }      
   }
   
