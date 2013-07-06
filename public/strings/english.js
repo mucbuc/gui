4752e0882ad1cbf058d7982187ad6b7222bda804
markymark <smallitle@hotmail.com>
2013-07-06 13:29:19 -0700

    moved strings

---
+++ public/strings/english.js
@@ -0,0 +1,18 @@
+(function() {
+	
+	var english = {
+		pause: 'Pause',
+		play: 'Play',
+		reset: 'Reset',
+		resetQuestion: 'Are you sure you want to Reset?',
+		yes: 'Yes',
+		no: 'No',
+		language: 'Language',
+		english: 'English',
+		german: 'German',
+		back: 'Back'
+	};
+
+	exports.english = english;
+
+})();
