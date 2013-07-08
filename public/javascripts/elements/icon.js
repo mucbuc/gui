1bc74780b3d1905959bf104ed6ba6c7601e62415
markymark <smallitle@hotmail.com>
2013-07-08 04:13:29 -0700

    fixed icon bug

--- public/javascripts/elements/icon.js
+++ public/javascripts/elements/icon.js
@@ -16,7 +16,6 @@ objective:
 
     controller.once( 'unload', function() {
       controller.removeListener( 'render', render );
-      controller.removeListener( 'update', update );
       delete instance;
     } );
     
@@ -25,23 +24,14 @@ objective:
   	img.src = controller.model;
     
     img.onload = function() {
-      controller.on( 'update', update );
       loaded = true;
     };
-    
-    function update() { 
-      if (img.src != controller.model) {
-        img.src = controller.model;
-        loaded = false;
-      }
-    }
     
     function render() {
       if (loaded) {
         var bounds = instance.bounds;
         controller.context.drawImage( img, bounds.left, bounds.top, bounds.width(), bounds.height() );
       }
-      // console.log( 'render icon' );
     }
   }
   
