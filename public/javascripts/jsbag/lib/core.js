f19117d7cef6be9375a254156086e8984148d4fe
markymark <smallitle@hotmail.com>
2013-07-28 04:46:18 -0700

    move jsbag

--- public/javascripts/jsbag/lib/core.js
+++ public/javascripts/jsbag/lib/core.js
@@ -1,3 +1,5 @@
+var exports = window;
+
 window.requestAnimFrame = (function() {
   var result = function(callback) {
     window.setTimeout( callback, 1000/60 );
@@ -5,4 +7,27 @@ window.requestAnimFrame = (function() {
   
   return result;
 })(); 
+
+// moved from /node_modules/utils.js
+(function(){
+
+  var cache = {};
+
+  function sign( a ) { 
+    return a < 0 ? -1 : (a > 0 ? 1 : 0);
+  }
+
+  function getTextWidth( text, context ) {
+
+  	if (!cache[text]) {
+  		cache[text] = context.measureText( text ).width;
+  	}
+
+  	return cache[text];
+  }
+
+  exports.sign = sign;
+  exports.getTextWidth = getTextWidth;
+
+})();
 
