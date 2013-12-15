7682cd2da73bde5bb2e7e905aa3b5e64aeaf4587
markymark <smallitle@hotmail.com>
2013-12-15 11:22:51 -0800

    paht fix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-exports.Batch = require( 'src/batch' ).Batch;
-exports.Emitter = require( 'src/emitter' ).Emitter;
-exports.Queue = require( 'src/queue' ).Queue;
-exports.Stream = require( 'src/stream' ).Stream;
+exports.Batch = require( './src/batch' ).Batch;
+exports.Emitter = require( './src/emitter' ).Emitter;
+exports.Queue = require( './src/queue' ).Queue;
+exports.Stream = require( './src/stream' ).Stream;
